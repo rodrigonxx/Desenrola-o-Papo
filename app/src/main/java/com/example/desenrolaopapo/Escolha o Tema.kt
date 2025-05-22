7a7f97d2fa6a3e8eff0ecfbc762b5774cf5e8677
Rodrigo Santos Machado <rodrigonx.dev@gmail.com>
2025-05-21 23:38:09 -0300

    Versao 2

--- app/src/main/java/com/example/desenrolaopapo/Escolha o Tema.kt
+++ app/src/main/java/com/example/desenrolaopapo/Escolha o Tema.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -13,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
